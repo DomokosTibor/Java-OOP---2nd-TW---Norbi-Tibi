9ddcfdb7da736855cbffcae0413100447bf8a942
dnorbert4000 <dnorbert@gmail.com>
2018-01-22 13:38:05 +0100

    simulation methods started

--- Simulation.java
+++ Simulation.java
@@ -1,3 +1,25 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 public class Simulation {
     
+
+    public Simulation() {
+
+    }
+
+    public int generateData() {
+        return 0;
+    }
+
+    public void load(){
+        PrintWriter pw = new PrintWriter(new File("data.csv"));
+        StringBuilder sb = new StringBuilder();
+        sb.append("number");
+        
+        pw.write(sb.toString());
+        pw.close();
+        System.out.println("done!");
+    }
 }
