70d5fcfa96ff3400e883411321d70ca328896b47
dnorbert4000 <dnorbert@gmail.com>
2018-01-22 11:51:16 +0100

    classes inhereitence setted up

--- Result.java
+++ Result.java
@@ -1,3 +1,3 @@
-public class Result {
+public class Result extends Simulation{
     
 }
--- Simulator.java
+++ Simulator.java
@@ -1,3 +1,3 @@
-public class Simulator {
+public class Simulator extends Simulation implements Logger{
     
 }
