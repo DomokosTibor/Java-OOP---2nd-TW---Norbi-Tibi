cfb911785e2dd730c490c4af486a04a48df10add
Domokos Tibor <Domokos.Tibor.82@gmail.com>
2018-01-23 14:21:43 +0100

    log method

--- Logger.java
+++ Logger.java
@@ -1,3 +1,11 @@
+import java.util.Date;
+
 public class Logger {
-    
+
+    public void log(String type, String message){
+        Date now = new Date();
+        now.getTime();
+        System.out.format("%s %s %s\n", now, type, message);
+    }
+
 }
