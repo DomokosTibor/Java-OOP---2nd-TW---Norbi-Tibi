d0ba06ae4ae89888d2ec7237d1a6e2f8d94f0f16
Domokos Tibor <Domokos.Tibor.82@gmail.com>
2018-01-23 14:41:11 +0100

    new contructors

--- Main.java
+++ Main.java
@@ -1,20 +1,35 @@
 public class Main {
 
-    public static int generateSimulation(int rounds) {
-        // Simulation goes here
-        System.out.println(rounds); 
-        return 0;
+    public static Simulation generateSimulation(int round) {
+        Simulation simulation = new Simulation(round);
+        Simulator simulator = new Simulator(simulation, new Logger(round, simulationNumber));
+        simulation.setRoundResult(simulator.run());
+        simulation.generateData("log.csv");
+        return simulation;
     }
 
     public static void main(String[] args) {
         if (args.length < 1) {
             // Show Stat
-            System.out.println("Stat..."); 
+            Statistics.getStat();
         }
         else {
             // Start simulation
-            int num = Integer.parseInt(args[0]);
-            generateSimulation(num);         
+            int simulationNumber = Integer.parseInt(args[0]);
+            int maxSimulationNumber = 1000;
+
+            if (simulationNumber > maxSimulationNumber || simulationNumber < 1) {
+                System.out.println("Number of simulations cannot be lower than 1 and bigger than " + maxSimulationNumber);
+                System.exit(-1);
+            }
+
+            Simulation[] simulations = new Simulation[simulationNumber];
+            for (int i = 0; i < simulationNumber; i++) {
+                simulations[i] = generateSimulation(i);
+            }
+
+
+            
         }
     }
 }
