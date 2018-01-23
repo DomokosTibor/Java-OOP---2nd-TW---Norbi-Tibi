ea5827999789d71fc7058b7940e913d927d198f6
Domokos Tibor <Domokos.Tibor.82@gmail.com>
2018-01-23 11:50:36 +0100

    generateData added

--- Simulation.java
+++ Simulation.java
@@ -2,17 +2,50 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.Formatter;
+
+
 public class Simulation {
     
 
     public Simulation() {
 
     }
-
-    public int generateData() {
-        return 0;
+    
+    public static void generateData(String filename, String roundData) {
+        File x = new File(filename);
+        if (x.exists()) {
+            /* DEL ME */
+            System.out.println(x.getName() + " exists!\nAppending to File...");
+            /* DEL ME */
+            try {
+                FileWriter fstream = new FileWriter(filename,true);
+                BufferedWriter fbw = new BufferedWriter(fstream);
+                fbw.write(roundData);
+                fbw.newLine();
+                fbw.close();
+            }
+            catch (Exception e) {
+                System.out.println("ERROR");
+            }
+        }
+        else {
+            /* DEL ME */
+            System.out.println(x.getName() + " NOT exists!\nCreating File...");
+            /* DEL ME */
+            try {
+                Formatter f = new Formatter(filename);
+                f.format(roundData + "\n");
+                f.close();
+            }
+            catch (Exception e) {
+                System.out.println("ERROR");
+            }
+        }
     }
-
+ 
     public void load(){
         PrintWriter pw = new PrintWriter(new File("data.csv"));
         StringBuilder sb = new StringBuilder();
