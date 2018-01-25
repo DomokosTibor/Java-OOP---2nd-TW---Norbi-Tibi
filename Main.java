ecca66b1d38a0269266c1922f954136f5a9d3033
Domokos Tibor <Domokos.Tibor.82@gmail.com>
2018-01-25 09:49:18 +0100

    NumberFormatException added

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
 public class Main {
 
     public static void generateSimulation(int round) {
-/*       
+/*
+        Simulation[] simulations = new Simulation[simulationNumber];}       
         Simulation simulation = new Simulation(round);
         Simulator simulator = new Simulator(simulation, new Logger(round, simulationNumber));
         simulation.setRoundResult(simulator.run());
@@ -32,15 +33,18 @@ public class Main {
             Statistics.getStat();
         }
         else {
-            int simulationNumber = Integer.parseInt(args[0]);
             int maxSimulationNumber = 1000000;
-
-            if (simulationNumber > maxSimulationNumber || simulationNumber < 1) {
-                Logger.displayMessage("Number of simulations cannot be lower than 1 and bigger than " + maxSimulationNumber);
-                System.exit(-1);
+            try {
+                int simulationNumber = Integer.parseInt(args[0]);
+                if (simulationNumber > maxSimulationNumber || simulationNumber < 1) {
+                    Logger.displayMessage("Number of simulations cannot be lower than 1 and bigger than " + maxSimulationNumber);
+                    System.exit(-1);
+                }
+                generateSimulation(simulationNumber);
             }
-//            Simulation[] simulations = new Simulation[simulationNumber];}
-            generateSimulation(simulationNumber);
+            catch(NumberFormatException e) {
+                Logger.displayMessage("The number of simulation has to be a NUMBER between 1 and " + maxSimulationNumber + "!");
+            }
             
         }
     }
